4d3d8202a8453c5f3ca8164f0dc4cde2defa2057
lstealer <hborchay@gmail.com>
2021-08-17 14:10:17 +0700

    first commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+production:
+  adapter: "mysql2"
+  host: "192.*.*.*"
+  username: "bii"
+  database: "bii_redmine"
+  password: "bii123"
+  encoding: "utf8"
---
+++ docker_install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+yum install -y yum-utils
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io
+systemctl start docker
+
